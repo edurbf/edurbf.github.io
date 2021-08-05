85d7b14b81db4471629dd42c24b1a62836456966
edurbf <baldinerjunior@gmail.com>
2021-08-05 01:53:20 -0300

    Update VRView_loader.js

--- docs/VRView_loader.js
+++ docs/VRView_loader.js
@@ -3,7 +3,7 @@ window.addEventListener('load', onVrViewLoad);
 function onVrViewLoad() {
   // Selector '#vrview' finds element with id 'vrview'.
   var vrView = new VRView.Player('#vrview', {
-    image: 'https://github.com/edurbf/Home/blob/master/docs/good_color_sphere.png',
+    image: './good_color_sphere.png',
     is_stereo: true
   });
 }
